073c347791843c04a1e2d01b88b2b53a02f7491b
Davi da Silva Böger <dsboger@gmail.com>
2016-11-11 12:36:37 -0200

    Remove more unused imports

--- src/extension.js
+++ src/extension.js
@@ -16,10 +16,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-const Lang = imports.lang;
-
-const Shell = imports.gi.Shell;
-
 const SwitcherPopup = imports.ui.switcherPopup;
 const AltTab = imports.ui.altTab;
 const AppFavorites = imports.ui.appFavorites;
